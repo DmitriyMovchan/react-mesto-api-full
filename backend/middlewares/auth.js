0d051cf4d58d1ad4dc7cf7519ea3c33912b794c2
Dmitriy Movchan <heavysky@yandex.ru>
2022-06-18 22:07:26 +0300

    env1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,7 +2,9 @@ const jwt = require('jsonwebtoken');
 
 const { UnauthorizedError } = require('../errors/UnauthorizedError');
 
-const JWT_SECRET_KEY = '1234567890';
+const { NODE_ENV, JWT_SECRET_KEY } = process.env;
+
+// const JWT_SECRET_KEY = '1234567890';
 const generateToken = (payload) => jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: '7d' });
 
 const isAuthorized = (req, res, next) => {
@@ -14,7 +16,7 @@ const isAuthorized = (req, res, next) => {
   try {
     // eslint-disable-next-line prefer-destructuring
     auth = auth.split(' ')[1];
-    decoded = jwt.verify(auth, JWT_SECRET_KEY);
+    decoded = jwt.verify(auth, NODE_ENV === 'production' ? JWT_SECRET_KEY : 'dev-secret');
   } catch (err) {
     // eslint-disable-next-line new-cap
     // eslint-disable-next-line prefer-promise-reject-errors
